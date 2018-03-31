b554261bbee83acb7580e16f5c61f9452271b69f
junahan <junahan@outlook.com>
2018-03-31 14:51:58 +0800

    Update the init-org.el

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -2,26 +2,79 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'org)
-(require 'ox-md nil t)
-(require 'org-latex nil t)
+;; Lots of stuff from - https://github.com/purcell/emacs.d/blob/master/lisp/init-org.el
+(when *is-a-mac*
+  (require 'grab-mac-link))
 
-;; org-mode 文本内语法高亮
-(setq org-src-fontify-natively t)
+(require 'org-cliplink)
 
-;; set default agend files
-(setq org-agenda-files '("~/JUNAHAN-A" ))
+(define-key global-map (kbd "C-c l") 'org-store-link)
+(define-key global-map (kbd "C-c a") 'org-agenda)
 
-;; org-agenda key binding
-(global-set-key (kbd "C-c a") 'org-agenda)
+;; Various preferences
+(setq org-log-done t
+      org-edit-timestamp-down-means-later t
+      org-archive-mark-done nil
+      org-hide-emphasis-markers t
+      org-catch-invisible-edits 'show
+      org-export-coding-system 'utf-8
+      org-fast-tag-selection-single-key 'expert
+      org-html-validation-link nil
+      org-export-kill-product-buffer-when-displayed t
+      org-tags-column 80)
 
 (global-visual-line-mode 1)
 
+(setq org-src-fontify-natively t)
+(setq org-support-shift-select t)
+
+(setq org-directory "~/org")
+(setq org-default-notes-file "~/org/refile.org")
+
+;;; Capturing
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      `(("t" "todo" entry (file "")  ; "" => `org-default-notes-file'
+         "* NEXT %?\n%U\n" :clock-resume t)
+        ("n" "note" entry (file "")
+         "* %? :NOTE:\n%U\n%a\n" :clock-resume t)
+        ))
+
+
+
+;; for image-model
 (eval-after-load 'image '(require 'image+))
 ;; auto adjust the image to fit the frame.
 (eval-after-load 'image+ '(imagex-auto-adjust-mode 1))
+(eval-after-load 'org '(require 'ox-md nil t))
+(eval-after-load 'org '(require 'org-latex nil t))
+
+(after-load 'org
+  (define-key org-mode-map (kbd "C-M-<up>") 'org-up-element)
+  (when *is-a-mac*
+    (define-key org-mode-map (kbd "M-h") nil)
+    (define-key org-mode-map (kbd "C-c g") 'org-mac-grab-link)))
+
+(after-load 'org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   `((R . t)
+     (ditaa . t)
+     (dot . t)
+     (emacs-lisp . t)
+     (gnuplot . t)
+     (haskell . nil)
+     (latex . t)
+     (ledger . t)
+     (ocaml . nil)
+     (octave . t)
+     (plantuml . t)
+     (python . t)
+     (ruby . t)
+     (screen . nil)
+     (,(if (locate-library "ob-sh") 'sh 'shell) . t)
+     (sql . nil)
+     (sqlite . t))))
 
 (provide 'init-org)
-
 ;;; init-org.el ends here
-
